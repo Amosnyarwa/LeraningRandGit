c5883a76d37154eb3ba3bab9c37665ca51558f38
LAP095\galen <amos.nyarwaya@gmail.com>
2021-03-31 17:26:39 +0300

    settlement sampling points

--- generating_sample_points.R
+++ generating_sample_points.R
@@ -2,5 +2,21 @@ library(sf)
 library(tidyverse)
 library(lwgeom)
  # read csv
-
 settlement_data <- read_csv(file = "inputs/Total_hhs_03_2019.csv")
+
+# read shapefile
+settlement_shape <- st_read("inputs/Settlements_2019.shp", crs=4326)
+sample_per_settlement <- rep(100, nrow(settlement_shape))
+
+df_sample <- settlement_shape %>% 
+  st_sample(sample_per_settlement)
+
+
+
+
+ggplot() + 
+  geom_sf(data = settlement_shape) + 
+  geom_sf(data = df_sample)
+
+?read_csv()
+?join()
